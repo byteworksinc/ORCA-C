f3431f8771afeeeaffec2de45bf3d176e754c2e3
Stephen Heumann <stephenheumann@gmail.com>
2017-10-21 20:47:34 -0500

    Fix types of CHAR_MAX and UCHAR_MAX in <limits.h>.
    These should be of type int (not unsigned int), since that is what char and unsigned char now promote to.

--- ORCACDefs/limits.h
+++ ORCACDefs/limits.h
@@ -14,7 +14,7 @@
 #define __limits__
 
 #define CHAR_BIT        8
-#define CHAR_MAX        255u
+#define CHAR_MAX        255
 #define CHAR_MIN        0
 #define SHRT_MAX        32767
 #define SHRT_MIN        (-32767-1)
@@ -25,7 +25,7 @@
 #define MB_LEN_MAX      1
 #define SCHAR_MAX       127
 #define SCHAR_MIN       (-128)
-#define UCHAR_MAX       255u
+#define UCHAR_MAX       255
 #define UINT_MAX        65535u
 #define ULONG_MAX       4294967295u
 #define USHRT_MAX       65535u
