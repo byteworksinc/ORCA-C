38345e4ca21f3245277a76981df9968cbd46fe35
Stephen Heumann <stephenheumann@gmail.com>
2017-12-13 23:05:20 -0600

    Update ORCA/C version number to 2.2.0 B2.

--- CCommon.pas
+++ CCommon.pas
@@ -104,7 +104,7 @@ const
    flag_t       = $00001000;            {treat all errors as terminal?}
    flag_w       = $00000200;            {wait when an error is found?}
 
-   versionStr = '2.2.0 B1';		{compiler version}
+   versionStr = '2.2.0 B2';		{compiler version}
 
 type
                                         {Misc.}
