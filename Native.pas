byteworksinc/ORCA-C

commit a3170ea715363004f3cf690d53931bdef5daf765

Do a few more native code peephole optimizations.
Patch from Kelvin Sherlock.

Native.pas
@@ -1422,6 +1422,19 @@ var
                Remove(ns+1);
                end; {if}
 
+         m_eor_imm:
+            if npeep[ns+1].opcode = m_eor_imm then begin
+               operand := operand ! npeep[ns+1].operand;
+               Remove(ns+1);
+               end; {if}
+
+         m_ora_imm:
+            if npeep[ns+1].opcode = m_ora_imm then begin
+               operand := operand | npeep[ns+1].operand;
+               Remove(ns+1);
+               end; {if}
+
+
          m_asl_a:
             if npeep[ns+1].opcode = m_tay then
                if npeep[ns+2].opcode = m_iny then
@@ -1724,6 +1737,16 @@ var
         	  Remove(ns);
         	  Remove(ns);
         	  end; {if}
+
+         { kws }
+         { stz $xx, stz $xx }
+         m_stz_abs, m_stz_absX, m_stz_dir, m_stz_dirX:
+            if npeep[ns].opcode = npeep[ns+1].opcode then begin
+               if npeep[ns].operand = npeep[ns+1].operand then begin
+                  Remove(ns+1)
+               end; {if}
+
+            end; {if}
 
          otherwise: ;
 

commit 5312843b934a7d240dce173d830f669a457593b5

Don't invalidly eliminate certain stores of 0 to global/static variables.
This could happen in native-code peephole optimization if two stz instructions targeting different global/static locations occurred consecutively.

This was a regression introduced by commit a3170ea7.

The following program demonstrates the problem:

#pragma optimize 1+2+8+64
int i,j=1;
int main (void) {
    i = 0;
    j = 0;
    return j; /* should return 0 */
}

Native.pas
@@ -1740,13 +1740,16 @@ var
 
          { kws }
          { stz $xx, stz $xx }
-         m_stz_abs, m_stz_absX, m_stz_dir, m_stz_dirX:
-            if npeep[ns].opcode = npeep[ns+1].opcode then begin
-               if npeep[ns].operand = npeep[ns+1].operand then begin
-                  Remove(ns+1)
-               end; {if}
-
-            end; {if}
+         m_stz_abs, m_stz_absX:
+            if npeep[ns].opcode = npeep[ns+1].opcode then
+               if npeep[ns].operand = npeep[ns+1].operand then
+                  if npeep[ns].name = npeep[ns+1].name then
+                     Remove(ns+1);
+         
+         m_stz_dir, m_stz_dirX:
+            if npeep[ns].opcode = npeep[ns+1].opcode then
+               if npeep[ns].operand = npeep[ns+1].operand then
+                  Remove(ns+1);
 
          otherwise: ;
 