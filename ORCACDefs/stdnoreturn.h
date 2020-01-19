bc1f6eb457b60b7aeafd2a69ff786fdcabf0c671
Stephen Heumann <stephenheumann@gmail.com>
2020-01-19 12:44:20 -0600

    Add <stdnoreturn.h> header.

---
+++ ORCACDefs/stdnoreturn.h
@@ -0,0 +1,12 @@
+/****************************************************************
+*
+*  stdnoreturn.h - macro for _Noreturn
+*
+****************************************************************/
+
+#ifndef __stdnoreturn__
+#define __stdnoreturn__
+
+#define noreturn _Noreturn
+
+#endif
