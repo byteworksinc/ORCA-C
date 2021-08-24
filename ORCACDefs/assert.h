08dbe1eea3593cf640d3ad45b74ecb09cc9967d4
Stephen Heumann <stephenheumann@gmail.com>
2021-08-24 18:35:01 -0500

    Include the function name in assertion failure messages.
    This is required by C99 and later, enabled by the availability of __func__.
    
    This requires an updated assertion-printing function in ORCALib. Unfortunately, GNO has the assertion-printing function in its libc rather than in ORCALib, because it calls the GNO implementation of stdio. Therefore, we continue to use the old form under GNO for now, to maintain compatibility with its existing libc.

--- ORCACDefs/assert.h
+++ ORCACDefs/assert.h
@@ -15,15 +15,20 @@
 #endif
 
 #ifndef NDEBUG
-#define assert(expression) (expression) ? ((void) 0) : (__assert(__FILE__, __LINE__, #expression))
+#ifndef __GNO__
+#define assert(expression) (expression) ? ((void) 0) : (__assert2(__FILE__, __LINE__, __func__, #expression))
 #else
-#define assert(expression)  ((void) 0)
+#define assert(expression) (expression) ? ((void) 0) : (__assert(__FILE__, __LINE__, #expression))
+#endif
+#else
+#define assert(expression)  ((void)0)
 #endif
 
 #ifndef __assert__
 #define __assert__
 
-extern void __assert(char *, int, char *);
+extern void __assert(const char *, unsigned, const char *);
+extern void __assert2(const char *, unsigned, const char *, const char *);
 
 #define static_assert _Static_assert
 
