byteworksinc/ORCA-C

commit 5871820e0c1ec3c17fb92df7e263f26a0eff0917

Support UTF-8/16/32 string literals and character constants (C11).
These have u8, u, or U prefixes, respectively. The types char16_t and char32_t (defined in <uchar.h>) are used for UTF-16 and UTF-32 code points.

Parser.pas
@@ -1909,7 +1909,7 @@ var
          if tree^.token.kind = plusch then begin
             rtree := tree^.right;
             if rtree^.token.kind in
-               [intconst,uintconst,charconst,scharconst,ucharconst] then
+               [intconst,uintconst,ushortconst,charconst,scharconst,ucharconst] then
                size := rtree^.token.ival
             else if rtree^.token.kind in [longconst,ulongconst] then
                size := rtree^.token.lval
@@ -2086,7 +2086,8 @@ var
             end;
 
          pointerType:
-            if etype = stringTypePtr then begin
+            if (etype = stringTypePtr) or (etype = utf16StringTypePtr)
+               or (etype = utf32StringTypePtr) then begin
                iPtr^.isConstant := true;
                iPtr^.iType := ccPointer;
                iPtr^.pval := 0;
@@ -2151,12 +2152,12 @@ var
             operator := tree^.token.kind;
             while operator in [plusch,minusch] do begin
                with tree^.right^.token do
-                  if kind in [intConst,uintconst,longConst,ulongconst,
-                     longlongConst,ulonglongconst,charconst,scharconst,
-                     ucharconst] then begin
+                  if kind in [intConst,uintconst,ushortconst,longConst,
+                     ulongconst,longlongConst,ulonglongconst,charconst,
+                     scharconst,ucharconst] then begin
                      if kind in [intConst,charconst,scharconst,ucharconst] then
                         offSet2 := ival
-                     else if kind = uintConst then
+                     else if kind in [uintConst,ushortconst] then
                         offset2 := ival & $0000ffff
                      else if kind in [longConst,ulongconst] then begin
                         offset2 := lval;
@@ -2322,6 +2323,8 @@ var
       kind: typeKind;                   {base type of an initializer}
       ktp: typePtr;			{array type with definedTypes removed}
       lPrintMacroExpansions: boolean;   {local copy of printMacroExpansions}
+      stringElementType: typePtr;       {element type of string literal}
+      stringLength: integer;            {elements in a string literal}
 
 
       procedure Fill (count: longint; tp: typePtr);
@@ -2450,44 +2453,61 @@ var
       kind := ktp^.kind;
 
       {handle string constants}
-      if (token.kind = stringConst) and (kind = scalarType)
-         and (ktp^.baseType in [cgByte,cgUByte]) then begin
-         if tp^.elements = 0 then begin
-            tp^.elements := token.sval^.length + 1;
-            RecomputeSizes(variable^.itype);
-            end {if}
-         else if tp^.elements < token.sval^.length then begin
-            Error(44);
-            errorFound := true;
-            end; {else if}
-         with ktp^ do begin
-            iPtr := pointer(Malloc(sizeof(initializerRecord)));
-            iPtr^.next := variable^.iPtr;
-            variable^.iPtr := iPtr;
-            iPtr^.count := 1;
-            iPtr^.bitdisp := 0;
-            iPtr^.bitsize := 0;
-            iPtr^.isStructOrUnion := false;
-            if (variable^.storage in [external,global,private]) then begin
-               iPtr^.isConstant := true;
-               iPtr^.itype := cgString;
-               iPtr^.sval := token.sval;
-               count := tp^.elements - token.sval^.length;
-               if count <> 0 then
-                  Fill(count, sCharPtr);
+      if token.kind = stringConst then begin
+         stringElementType := StringType(token.prefix)^.aType;
+         if (kind = scalarType) and 
+            (((ktp^.baseType in [cgByte,cgUByte])
+               and (stringElementType = charPtr))
+            or CompTypes(ktp,stringElementType)) then begin
+            stringLength := token.sval^.length div ord(stringElementType^.size);
+            if tp^.elements = 0 then begin
+               tp^.elements := stringLength;
+               RecomputeSizes(variable^.itype);
                end {if}
-            else begin
-               iPtr^.isConstant := false;
-               new(ep);
-               iPtr^.iTree := ep;
-               ep^.next := nil;
-               ep^.left := nil;
-               ep^.middle := nil;
-               ep^.right := nil;
-               ep^.token := token;
-               end; {else}
-            end; {with}
-         NextToken;
+            else if tp^.elements < stringLength-1 then begin
+               Error(44);
+               errorFound := true;
+               end; {else if}
+            with ktp^ do begin
+               iPtr := pointer(Malloc(sizeof(initializerRecord)));
+               iPtr^.next := variable^.iPtr;
+               variable^.iPtr := iPtr;
+               iPtr^.count := 1;
+               iPtr^.bitdisp := 0;
+               iPtr^.bitsize := 0;
+               iPtr^.isStructOrUnion := false;
+               if (variable^.storage in [external,global,private]) then begin
+                  iPtr^.isConstant := true;
+                  iPtr^.itype := cgString;
+                  iPtr^.sval := token.sval;
+                  count := tp^.elements - stringLength;
+                  if count > 0 then
+                     Fill(count, stringElementType)
+                  else if count = -1 then begin
+                     iPtr^.sval := pointer(GMalloc(token.sval^.length+2));
+                     CopyLongString(iPtr^.sval, token.sval);
+                     iPtr^.sval^.length :=
+                        iPtr^.sval^.length - ord(stringElementType^.size);
+                     end; {else if}
+                  end {if}
+               else begin
+                  iPtr^.isConstant := false;
+                  new(ep);
+                  iPtr^.iTree := ep;
+                  ep^.next := nil;
+                  ep^.left := nil;
+                  ep^.middle := nil;
+                  ep^.right := nil;
+                  ep^.token := token;
+                  end; {else}
+               end; {with}
+            NextToken;
+            end {if}
+         else begin
+            Error(47);
+            errorFound := true;
+            NextToken;
+            end; {else}
          end {if}
 
       {handle arrays of non-strings}
@@ -3643,7 +3663,8 @@ if isFunction then begin
             NextToken;
             Match(lparench,13);
             if token.kind in
-               [intconst,uintconst,charconst,scharconst,ucharconst] then begin
+               [intconst,uintconst,ushortconst,charconst,scharconst,ucharconst]
+               then begin
                toolNum := token.ival;
                NextToken;
                end {if}
@@ -3655,7 +3676,8 @@ if isFunction then begin
                NextToken;
                end {if}
             else if token.kind in
-               [intconst,uintconst,charconst,scharconst,ucharconst] then begin
+               [intconst,uintconst,ushortconst,charconst,scharconst,ucharconst]
+               then begin
                dispatcher := token.ival;
                NextToken;
                end {if}
@@ -4352,7 +4374,8 @@ var
          elements := itype^.elements;
          if elements = 0 then goto 1;   {don't init flexible array member}
          if iPtr^.iTree^.token.kind = stringConst then begin
-            size := iPtr^.iTree^.token.sval^.length+1;
+            elements := elements * itype^.aType^.size;
+            size := iPtr^.iTree^.token.sval^.length;
             if size >= elements then
                size := ord(elements)
             else

CCommon.pas
@@ -172,7 +172,7 @@ type
                                         { They are created only by casts.    }
                intconst,uintconst,longconst,ulongconst,longlongconst,
                ulonglongconst,floatconst,doubleconst,extendedconst,compconst,
-               charconst,scharconst,ucharconst,stringconst,
+               charconst,scharconst,ucharconst,ushortconst,stringconst,
                                         {reserved words}
                _Alignassy,_Alignofsy,_Atomicsy,_Boolsy,_Complexsy,
                _Genericsy,_Imaginarysy,_Noreturnsy,_Static_assertsy,_Thread_localsy,
@@ -210,6 +210,9 @@ type
        ch_asterisk,ch_slash,ch_percent,ch_carot,ch_pound,ch_colon,
        ch_backslash,letter,digit);
 
+                                        {prefixes of a character/string literal}
+   charStrPrefixEnum = (prefix_none,prefix_L,prefix_u16,prefix_U32,prefix_u8);
+
    tokenSet = set of tokenEnum;
    tokenClass = (reservedWord,reservedSymbol,identifier,intConstant,longConstant,
                  longlongConstant,realConstant,stringConstant,macroParameter);
@@ -227,7 +230,8 @@ type
          longlongConstant: (qval: longlong);
          realConstant  : (rval: extended);
          stringConstant: (sval: longstringPtr;
-                          ispstring: boolean);
+                          ispstring: boolean;
+                          prefix: charStrPrefixEnum);
          macroParameter: (pnum: integer);
      end;
  

CGI.Comments
@@ -407,7 +407,8 @@
 {  GenS(pc_lca, str)                                            }
 {                                                               }
 {  Loads the address of a string onto the stack.  Str is a      }
-{  pointer to a string constant.                                }
+{  pointer to a string constant.  No null terminator is added;  }
+{  it should be explicitly included in str if desired.          }
 {                                                               }
 {                                                               }
 {  pc_lda - load a local address                                }

Charset.pas
@@ -28,6 +28,14 @@ const
 
 type
    ucsCodePoint = 0..maxUCSCodePoint;
+   utf8Rec = record
+      length: integer;
+      bytes: packed array [1..4] of byte;
+      end;
+   utf16Rec = record
+      length: integer;
+      codeUnits: packed array [1..2] of integer;
+      end;
 
 
 function ConvertMacRomanToUCS(ch: char): ucsCodePoint;
@@ -36,6 +44,7 @@ function ConvertMacRomanToUCS(ch: char): ucsCodePoint;
 {                                                                }
 { Returns UCS code point value for the character.                }
 
+
 function ConvertUCSToMacRoman(ch: ucsCodePoint): integer;
 
 { convert a character from UCS (Unicode) to MacRoman charset     }
@@ -43,12 +52,31 @@ function ConvertUCSToMacRoman(ch: ucsCodePoint): integer;
 { Returns ordinal value of the character, or -1 if it can't be   }
 { converted.                                                     }
 
+
+procedure UTF16Encode(ch: ucsCodePoint; var utf16: utf16Rec);
+
+{ Encode a UCS code point in UTF-16                              }
+{                                                                }
+{ ch - the code point                                            }
+{ utf16 - set to the UTF-16 representation of the code point     }
+
+
+procedure UTF8Encode(ch: ucsCodePoint; var utf8: utf8Rec);
+
+{ Encode a UCS code point in UTF-8                               }
+{                                                                }
+{ ch - the code point                                            }
+{ utf16 - set to the UTF-8 representation of the code point      }
+
+
 function ValidUCNForIdentifier(ch: ucsCodePoint; initial: boolean): boolean;
 
 { Check if a code point is valid for a UCN in an identifier      }
 {                                                                }
 { ch - the code point                                            }
 { initial - is this UCN the initial element of the identifier?   }
+
+{----------------------------------------------------------------}
 
 implementation
 
@@ -98,6 +126,60 @@ else begin
    end; {else}
 1:
 end; {ConvertUCSToMacRoman}
+
+
+procedure UTF16Encode{ch: ucsCodePoint; var utf16: utf16Rec};
+
+{ Encode a UCS code point in UTF-16                              }
+{                                                                }
+{ ch - the code point                                            }
+{ utf16 - set to the UTF-16 representation of the code point     }
+
+begin {UTF16Encode}
+if ch <= $00ffff then begin
+   utf16.length := 1;
+   utf16.codeUnits[1] := ord(ch);
+   end {if}
+else begin
+   utf16.length := 2;
+   ch := ch - $010000;
+   utf16.codeUnits[1] := $D800 | ord(ch >> 10);
+   utf16.codeUnits[2] := $DC00 | ord(ch & $03ff);
+   end; {else}
+end; {UTF16Encode}
+
+
+procedure UTF8Encode{ch: ucsCodePoint; var utf8: utf8Rec};
+
+{ Encode a UCS code point in UTF-8                               }
+{                                                                }
+{ ch - the code point                                            }
+{ utf16 - set to the UTF-8 representation of the code point      }
+
+begin {UTF8Encode}
+if ch <= $00007f then begin
+   utf8.length := 1;
+   utf8.bytes[1] := ord(ch);
+   end {if}
+else if ch <= $0007ff then begin
+   utf8.length := 2;
+   utf8.bytes[1] := $C0 | ord(ch >> 6);
+   utf8.bytes[2] := $80 | ord(ch & $3f)
+   end {else if}
+else if ch <= $00ffff then begin
+   utf8.length := 3;
+   utf8.bytes[1] := $E0 | ord(ch >> 12);
+   utf8.bytes[2] := $80 | ord((ch >> 6) & $3f);
+   utf8.bytes[3] := $80 | ord(ch & $3f);
+   end {else if}
+else begin
+   utf8.length := 4;
+   utf8.bytes[1] := $F0 | ord(ch >> 18);
+   utf8.bytes[2] := $80 | ord((ch >> 12) & $3f);
+   utf8.bytes[3] := $80 | ord((ch >> 6) & $3f);
+   utf8.bytes[4] := $80 | ord(ch & $3f);
+   end; {else}
+end; {UTF8Encode}
 
 
 function ValidUCNForIdentifier{(ch: ucsCodePoint; initial: boolean): boolean};

Expression.pas
@@ -1046,7 +1046,7 @@ var
       begin {RealVal}
       if token.kind in [intconst,charconst,scharconst,ucharconst] then
          RealVal := token.ival
-      else if token.kind = uintconst then begin
+      else if token.kind in [uintconst,ushortconst] then begin
          if token.ival < 0 then
             RealVal := (token.ival & $7FFF) + 32768.0
          else
@@ -1076,7 +1076,7 @@ var
       begin {IntVal}
       if token.kind in [intconst,charconst,scharconst,ucharconst] then
          IntVal := token.ival
-      else if token.kind = uintconst then begin
+      else if token.kind in [uintconst,ushortconst] then begin
          IntVal := token.ival & $0000FFFF;
          end {else if}
       else {if token.kind in [longconst,ulongconst] then} begin
@@ -1097,7 +1097,7 @@ var
          else
             result.hi := 0;
          end {if}
-      else if token.kind = uintconst then begin
+      else if token.kind in [uintconst,ushortconst] then begin
          result.lo := token.ival & $0000FFFF;
          result.hi := 0;
          end {else if}
@@ -1125,7 +1125,7 @@ var
       begin {PPKind}
       if token.kind in [intconst,longconst] then
          PPKind := longlongconst
-      else if token.kind in [uintconst,ulongconst] then
+      else if token.kind in [uintconst,ushortconst,ulongconst] then
          PPKind := ulonglongconst
       else
          PPKind := token.kind;
@@ -1161,13 +1161,13 @@ var
          op^.right := Pop;
          op^.middle := Pop;
          op^.left := Pop;
-         if op^.right^.token.kind in [intconst,uintconst,
+         if op^.right^.token.kind in [intconst,uintconst,ushortconst,
             longconst,ulongconst,longlongconst,ulonglongconst,
             charconst,scharconst,ucharconst] then
-            if op^.left^.token.kind in [intconst,uintconst,
+            if op^.left^.token.kind in [intconst,uintconst,ushortconst,
                longconst,ulongconst,longlongconst,ulonglongconst,
                charconst,scharconst,ucharconst] then
-               if op^.middle^.token.kind in [intconst,uintconst,
+               if op^.middle^.token.kind in [intconst,uintconst,ushortconst,
                   longconst,ulongconst,longlongconst,ulonglongconst,
                   charconst,scharconst,ucharconst] then begin
                   GetLongLongVal(llop1, op^.left^.token);
@@ -1211,9 +1211,9 @@ var
          op^.left := Pop;
          kindRight := op^.right^.token.kind;
          kindLeft := op^.left^.token.kind;
-         if kindRight in [intconst,uintconst,longconst,ulongconst,
+         if kindRight in [intconst,uintconst,ushortconst,longconst,ulongconst,
             charconst,scharconst,ucharconst] then begin
-            if kindLeft in [intconst,uintconst,longconst,ulongconst,
+            if kindLeft in [intconst,uintconst,ushortconst,longconst,ulongconst,
                charconst,scharconst,ucharconst] then begin
                if kind = preprocessorExpression then
                   goto 2;
@@ -1223,7 +1223,8 @@ var
                   ekind := ulongconst
                else if (kindRight = longconst) or (kindLeft = longconst) then
                   ekind := longconst
-               else if (kindRight = uintconst) or (kindLeft = uintconst) then
+               else if (kindRight = uintconst) or (kindLeft = uintconst) 
+                  or (kindRight = ushortconst) or (kindLeft = ushortconst) then
                   ekind := uintconst
                else
                   ekind := intconst;
@@ -1289,7 +1290,8 @@ var
                                 ekind := kindLeft;
                                 end;
                   gtgtop      : begin                                   {>>}
-                                if kindLeft in [uintconst,ulongconst] then
+                                if kindLeft in [uintconst,ushortconst,ulongconst]
+                                   then
                                    op1 := lshr(op1,op2)
                                 else
                                    op1 := op1 >> op2;
@@ -1350,10 +1352,10 @@ var
                end; {if}
             end; {if}
 2:
-         if kindRight in [intconst,uintconst,longconst,ulongconst,
+         if kindRight in [intconst,uintconst,ushortconst,longconst,ulongconst,
             longlongconst,ulonglongconst,charconst,scharconst,ucharconst]
             then begin
-            if kindLeft in [intconst,uintconst,longconst,ulongconst,
+            if kindLeft in [intconst,uintconst,ushortconst,longconst,ulongconst,
                longlongconst,ulonglongconst,charconst,scharconst,ucharconst]
                then begin
 
@@ -1503,10 +1505,10 @@ var
 
          if op^.right^.token.kind in [intconst,uintconst,longconst,ulongconst,
             longlongconst,ulonglongconst,floatconst,doubleconst,extendedconst,
-            compconst,charconst,scharconst,ucharconst] then
+            compconst,charconst,scharconst,ucharconst,ushortconst] then
             if op^.left^.token.kind in [intconst,uintconst,longconst,ulongconst,
                longlongconst,ulonglongconst,floatconst,doubleconst,extendedconst,
-               compconst,charconst,scharconst,ucharconst] then
+               compconst,charconst,scharconst,ucharconst,ushortconst] then
                begin
                if fenvAccess then
                   if kind in [normalExpression, autoInitializerExpression] then
@@ -1601,7 +1603,7 @@ var
             op^.token.kind := ulongConst;
             op^.token.class := longConstant;
             if op^.left^.token.kind = stringConst then
-               op^.token.lval := op^.left^.token.sval^.length+1
+               op^.token.lval := op^.left^.token.sval^.length
             else begin
                lCodeGeneration := codeGeneration;
                codeGeneration := false;
@@ -1728,7 +1730,7 @@ var
             begin
             if (kind <> preprocessorExpression) and (op^.left^.token.kind
                in [intconst,uintconst,longconst,ulongconst,charconst,scharconst,
-               ucharconst]) then begin
+               ucharconst,ushortconst]) then begin
 
                {evaluate a constant operation}
                ekind := op^.left^.token.kind;
@@ -1759,7 +1761,7 @@ var
                end {if}
             else if op^.left^.token.kind in [longlongconst,ulonglongconst,
                intconst,uintconst,longconst,ulongconst,charconst,scharconst,
-               ucharconst] then begin
+               ucharconst,ushortconst] then begin
                
                {evaluate a constant operation with long long operand}
                ekind := op^.left^.token.kind;
@@ -3561,7 +3563,7 @@ case tree^.token.kind of
          end; {case}
       end;
 
-   intConst,uintConst,charConst,scharConst,ucharConst: begin
+   intConst,uintConst,ushortConst,charConst,scharConst,ucharConst: begin
       Gen1t(pc_ldc, tree^.token.ival, cgWord);
       lastwasconst := true;
       lastconst := tree^.token.ival;
@@ -3569,13 +3571,15 @@ case tree^.token.kind of
          expressionType := intPtr
       else if tree^.token.kind = uintConst then
          expressionType := uIntPtr
+      else if tree^.token.kind = ushortConst then
+         expressionType := uShortPtr
       else if tree^.token.kind = charConst then
          expressionType := charPtr
       else if tree^.token.kind = scharConst then
          expressionType := scharPtr
       else {if tree^.token.kind = ucharConst then}
          expressionType := ucharPtr;
-      end; {case intConst,uintConst,charConst,scharConst,ucharConst}
+      end; {case intConst,uintConst,ushortConst,charConst,scharConst,ucharConst}
 
    longConst,ulongConst: begin
       GenLdcLong(tree^.token.lval);
@@ -3621,7 +3625,7 @@ case tree^.token.kind of
 
    stringConst: begin
       GenS(pc_lca, tree^.token.sval);
-      expressionType := stringTypePtr;
+      expressionType := StringType(tree^.token.prefix);
       end; {case stringConst}
 
    eqch: begin                          {=}
@@ -4685,11 +4689,12 @@ else begin                              {record the expression for an initialize
          while castValue^.token.kind = castoper do
             castValue := castValue^.left;
          if castValue^.token.kind in
-            [intconst,uintconst,charconst,scharconst,ucharconst] then begin
+            [intconst,uintconst,ushortconst,charconst,scharconst,ucharconst]
+            then begin
             expressionValue := castValue^.token.ival;
             isConstant := true;
             expressionType := tree^.castType;
-            if (castValue^.token.kind = uintconst)
+            if (castValue^.token.kind in [uintconst,ushortconst])
                or (expressionType^.kind = pointerType) then
                expressionValue := expressionValue & $0000FFFF;
             goto 1;
@@ -4714,10 +4719,13 @@ else begin                              {record the expression for an initialize
             expressionType := ucharPtr;
          isConstant := true;
          end {else if}
-      else if tree^.token.kind = uintconst then begin
+      else if tree^.token.kind in [uintconst,ushortconst] then begin
          expressionValue := tree^.token.ival;
          expressionValue := expressionValue & $0000FFFF;
-         expressionType := uIntPtr;
+         if tree^.token.kind = uintconst then
+            expressionType := uIntPtr
+         else {if tree^.token.kind = ushortconst then}
+            expressionType := uShortPtr;
          isConstant := true;
          end {else if}
       else if tree^.token.kind = longconst then begin
@@ -4773,7 +4781,7 @@ else begin                              {record the expression for an initialize
          end {else if}
       else if tree^.token.kind = stringconst then begin
          expressionValue := ord4(tree^.token.sval);
-         expressionType := stringTypePtr;
+         expressionType := StringType(tree^.token.prefix);
          isConstant := true;
          if kind in [arrayExpression,preprocessorExpression] then begin
             expressionType := intPtr;
@@ -4817,7 +4825,7 @@ procedure InitExpression;
 begin {InitExpression}
 startTerm := [ident,intconst,uintconst,longconst,ulongconst,longlongconst,
               ulonglongconst,floatconst,doubleconst,extendedconst,compconst,
-              charconst,scharconst,ucharconst,stringconst];
+              charconst,scharconst,ucharconst,ushortconst,stringconst];
 startExpression:= startTerm +
              [lparench,asteriskch,andch,plusch,minusch,excch,tildech,sizeofsy,
               plusplusop,minusminusop,typedef,_Alignofsy,_Genericsy];

Gen.pas
@@ -5890,11 +5890,10 @@ procedure GenTree {op: icptr};
    gLong.where := onStack;
    GenNative(m_pea, immediate, stringSize, nil, stringReference+shift16);
    GenNative(m_pea, immediate, stringSize, nil, stringReference);
-   if maxString-stringSize >= op^.q+1 then begin
+   if maxString-stringSize >= op^.q then begin
       for i := 1 to op^.q do
          stringSpace[i+stringSize] := op^.str^.str[i];
-      stringSpace[stringSize+op^.q+1] := chr(0);
-      stringSize := stringSize+op^.q+1;
+      stringSize := stringSize+op^.q;
       end
    else
       Error(cge3);

commit 7ac3fe642491452395d765cbe61fb9e0071cd0ad

Allow string constants in initializers for arrays of char*.
For example, declarations like the following should be accepted:

char *p[] = {"abc", "def"};

This previously worked, but it was broken by commit 5871820e0c.

Parser.pas
@@ -2491,64 +2491,58 @@ var
       kind := ktp^.kind;
 
       {handle string constants}
-      if token.kind = stringConst then begin
+      if token.kind = stringConst then
          stringElementType := StringType(token.prefix)^.aType;
-         if (kind = scalarType) and 
-            (((ktp^.baseType in [cgByte,cgUByte])
-               and (stringElementType = charPtr))
-            or CompTypes(ktp,stringElementType)) then begin
-            stringLength := token.sval^.length div ord(stringElementType^.size);
-            if tp^.elements = 0 then begin
-               tp^.elements := stringLength;
-               RecomputeSizes(variable^.itype);
-               end {if}
-            else if tp^.elements < stringLength-1 then begin
-               Error(44);
-               errorFound := true;
-               end; {else if}
-            with ktp^ do begin
-               iPtr := pointer(Malloc(sizeof(initializerRecord)));
-               iPtr^.next := variable^.iPtr;
-               variable^.iPtr := iPtr;
-               iPtr^.count := 1;
-               iPtr^.bitdisp := 0;
-               iPtr^.bitsize := 0;
-               iPtr^.isStructOrUnion := false;
-               if (variable^.storage in [external,global,private]) then begin
-                  iPtr^.isConstant := true;
-                  iPtr^.itype := cgString;
-                  iPtr^.sval := token.sval;
-                  count := tp^.elements - stringLength;
-                  if count > 0 then
-                     Fill(count, stringElementType)
-                  else if count = -1 then begin
-                     iPtr^.sval := pointer(GMalloc(token.sval^.length+2));
-                     CopyLongString(iPtr^.sval, token.sval);
-                     iPtr^.sval^.length :=
-                        iPtr^.sval^.length - ord(stringElementType^.size);
-                     end; {else if}
-                  end {if}
-               else begin
-                  iPtr^.isConstant := false;
-                  new(ep);
-                  iPtr^.iTree := ep;
-                  ep^.next := nil;
-                  ep^.left := nil;
-                  ep^.middle := nil;
-                  ep^.right := nil;
-                  ep^.token := token;
-                  end; {else}
-               end; {with}
-            NextToken;
+      if (token.kind = stringConst) and (kind = scalarType) and 
+         (((ktp^.baseType in [cgByte,cgUByte])
+            and (stringElementType = charPtr))
+         or CompTypes(ktp,stringElementType)) then begin
+         stringLength := token.sval^.length div ord(stringElementType^.size);
+         if tp^.elements = 0 then begin
+            tp^.elements := stringLength;
+            RecomputeSizes(variable^.itype);
             end {if}
-         else begin
-            Error(47);
+         else if tp^.elements < stringLength-1 then begin
+            Error(44);
             errorFound := true;
-            NextToken;
-            end; {else}
+            end; {else if}
+         with ktp^ do begin
+            iPtr := pointer(Malloc(sizeof(initializerRecord)));
+            iPtr^.next := variable^.iPtr;
+            variable^.iPtr := iPtr;
+            iPtr^.count := 1;
+            iPtr^.bitdisp := 0;
+            iPtr^.bitsize := 0;
+            iPtr^.isStructOrUnion := false;
+            if (variable^.storage in [external,global,private]) then begin
+               iPtr^.isConstant := true;
+               iPtr^.itype := cgString;
+               iPtr^.sval := token.sval;
+               count := tp^.elements - stringLength;
+               if count > 0 then
+                  Fill(count, stringElementType)
+               else if count = -1 then begin
+                  iPtr^.sval := pointer(GMalloc(token.sval^.length+2));
+                  CopyLongString(iPtr^.sval, token.sval);
+                  iPtr^.sval^.length :=
+                     iPtr^.sval^.length - ord(stringElementType^.size);
+                  end; {else if}
+               end {if}
+            else begin
+               iPtr^.isConstant := false;
+               new(ep);
+               iPtr^.iTree := ep;
+               ep^.next := nil;
+               ep^.left := nil;
+               ep^.middle := nil;
+               ep^.right := nil;
+               ep^.token := token;
+               end; {else}
+            end; {with}
+         NextToken;
          end {if}
 
-      {handle arrays of non-strings}
+      {handle arrays not initialized with a string constant}
       else if kind in
          [scalarType,pointerType,enumType,arrayType,structType,unionType] then
          begin