73a081bd55c0f9c38acfd5b379daf80b91af9a11
Stephen Heumann <stephenheumann@gmail.com>
2021-11-20 19:33:04 -0600

    Add header declarations and documentation for new math functions.

--- ORCACDefs/math.h
+++ ORCACDefs/math.h
@@ -24,6 +24,9 @@ typedef long double double_t;
 
 #define NAN (0.0F/0.0F)
 
+#define ILOGB0   (-32767-1)
+#define ILOGBNAN (-32767-1)
+
 #define FP_INFINITE  0xFE
 #define FP_NAN       0xFD
 #define FP_NORMAL    0x00
@@ -80,5 +83,48 @@ double          frexp(double, int *);
 double          ldexp(double, int);
 double          modf(double, double *);
 double          pow(double, double);
+
+double          cbrt(double);
+float           cbrtf(float);
+long double     cbrtl(long double);
+double          copysign(double, double);
+float           copysignf(float, float);
+long double     copysignl(long double, long double);
+double          exp2(double);
+float           exp2f(float);
+long double     exp2l(long double);
+double          expm1(double);
+float           expm1f(float);
+long double     expm1l(long double);
+int             ilogb(double);
+int             ilogbf(float);
+int             ilogbl(long double);
+double          log1p(double);
+float           log1pf(float);
+long double     log1pl(long double);
+double          log2(double);
+float           log2f(float);
+long double     log2l(long double);
+double          logb(double);
+float           logbf(float);
+long double     logbl(long double);
+long            lrint(double);
+long            lrintf(float);
+long            lrintl(long double);
+double          remainder(double, double);
+float           remainderf(float, float);
+long double     remainderl(long double, long double);
+double          remquo(double, double, int *);
+float           remquof(float, float, int *);
+long double     remquol(long double, long double, int *);
+double          rint(double);
+float           rintf(float);
+long double     rintl(long double);
+double          scalbn(double, int);
+float           scalbnf(float, int);
+long double     scalbnl(long double, int);
+double          trunc(double);
+float           truncf(float);
+long double     truncl(long double); 
 
 #endif
