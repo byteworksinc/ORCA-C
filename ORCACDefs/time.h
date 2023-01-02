54761189517f1c2e0b3f7144611b6cdb32caa218
Stephen Heumann <stephenheumann@gmail.com>
2023-01-01 21:46:19 -0600

    Add documentation and headers for timespec_get.
    A macro is used to control whether struct timespec is declared, because GNO might want to declare it in other headers, and this would allow it to avoid duplicate declarations. (This will still require changes in the GNO headers. Currently, they declare struct timespec with different field names, although the layout is the same.)

--- ORCACDefs/time.h
+++ ORCACDefs/time.h
@@ -28,11 +28,21 @@ struct tm {
         int tm_isdst;
         };
 
+#ifndef __struct_timespec__
+#define __struct_timespec__
+struct timespec {
+        time_t tv_sec;
+        long   tv_nsec;
+        };
+#endif
+
 clock_t         __clocks_per_sec(void);
 #ifndef __KeepNamespacePure__
    #define CLK_TCK      (__clocks_per_sec())
 #endif
 #define CLOCKS_PER_SEC  (__clocks_per_sec())
+
+#define TIME_UTC 1
 
 #ifndef NULL
 #define NULL  (void *) 0L
@@ -52,5 +62,6 @@ struct tm      *localtime(const time_t *);
 time_t          mktime(struct tm *);
 size_t          strftime(char *, size_t, const char *, const struct tm *);
 time_t          time(time_t *);
+int             timespec_get(struct timespec *, int);
 
 #endif
