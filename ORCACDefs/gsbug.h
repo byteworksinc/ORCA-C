d7cc9b59094706eb6a756c13b7e1679505933880
ksherlock <ksherlock@gmail.com>
2024-01-18 18:51:22 -0500

    update gsbug.h prototypes and errors based on gsbug and niftylist (#86)
    * update gsbug.h prototypes and errors based on gsbug and niftylist
    
    * updates
    
    * remove gsbug 1.7 development error codes

--- ORCACDefs/gsbug.h
+++ ORCACDefs/gsbug.h
@@ -14,13 +14,17 @@
 #ifndef __GSBUG__
 #define __GSBUG__
 
+/* Error Codes */
+#define debugUnImpErr 0xFF01
+#define debugBadSelErr 0xFF02
+
 #define dgiProgramCounter 0  /* for DebugGetInfo */
 
-extern pascal Word DebugVersion() inline(0x04FF,dispatcher);
-extern pascal Word DebugStatus() inline(0x06FF,dispatcher);
-extern pascal void DebugStr() inline(0x09FF,dispatcher);
-extern pascal void SetMileStone() inline(0x0AFF,dispatcher);
-extern pascal void DebugSetHook() inline(0x0BFF,dispatcher);
-extern pascal LongWord DebugGetInfo() inline(0x0CFF,dispatcher);
+extern pascal Word DebugVersion(void) inline(0x04FF,dispatcher);
+extern pascal Word DebugStatus(void) inline(0x06FF,dispatcher);
+extern pascal void DebugStr(Pointer) inline(0x09FF,dispatcher);
+extern pascal void SetMileStone(Pointer) inline(0x0AFF,dispatcher);
+extern pascal void DebugSetHook(VoidProcPtr) inline(0x0BFF,dispatcher);
+extern pascal LongWord DebugGetInfo(Word) inline(0x0CFF,dispatcher);
 
 #endif
