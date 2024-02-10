byteworksinc/ORCA-C

commit f2a66a524a23d265841f18ddbcc2755a3f460344

Fix several issues with evaluation of the ++ and -- operators.
These would generally not work correctly on bit-fields, or on floating-point values that were in a structure or were accessed via a pointer.

The below program is an example that would demonstrate problems:

#include <stdio.h>

int main(void) {
        struct {
                signed int i:7;
                unsigned long int j:6;
                _Bool b:1;
                double d;
        } s = {-10, -20, 0, 5.0};

        double d = 70.0, *dp = &d;

        printf("%i\n", (int)s.i++);
        printf("%i\n", (int)s.i--);
        printf("%i\n", (int)++s.i);
        printf("%i\n", (int)--s.i);
        printf("%i\n", (int)s.i);

        printf("%i\n", (int)s.j++);
        printf("%i\n", (int)s.j--);
        printf("%i\n", (int)++s.j);
        printf("%i\n", (int)--s.j);
        printf("%i\n", (int)s.j);

        printf("%i\n", s.b++);
        printf("%i\n", s.b--);
        printf("%i\n", ++s.b);
        printf("%i\n", --s.b);
        printf("%i\n", s.b);

        printf("%f\n", s.d++);
        printf("%f\n", s.d--);
        printf("%f\n", ++s.d);
        printf("%f\n", --s.d);
        printf("%f\n", s.d);

        printf("%f\n", (*dp)++);
        printf("%f\n", (*dp)--);
        printf("%f\n", ++*dp);
        printf("%f\n", --*dp);
        printf("%f\n", *dp);
}

Expression.pas
@@ -2368,10 +2368,11 @@ var
 
    begin {DoIncDec}
    L_Value(tree);
-   with tree^.id^ do
-      if (tree^.token.kind = ident)
-         and ((iType^.kind in [scalarType,pointerType])
-         or ((iType^.kind = arrayType) and (storage = parameter))) then begin
+   if (tree^.token.kind = ident)
+      and ((tree^.id^.iType^.kind in [scalarType,pointerType])
+      or ((tree^.id^.iType^.kind = arrayType) and (tree^.id^.storage = parameter)))
+      then
+      with tree^.id^ do begin
 
          {check for ++ or -- of a constant}
          if iType^.isConstant then
@@ -2459,62 +2460,64 @@ var
             otherwise: ;
             end; {case}
          expressionType := itype;
-         end {if}
-      else begin
+         end {with}
+   else begin
 
-         {do an indirect ++ or --}
-         LoadAddress(tree);             {get the address to save to}
-         if expressionType^.kind = arrayType then
-            expressionType := expressionType^.aType
-         else if expressionType^.kind = pointerType then
-            expressionType := expressionType^.pType;
-         if expressionType^.kind = scalarType then
-            if expressionType^.baseType in [cgByte,cgUByte,cgWord,cgUWord] then
-               tp := expressionType^.baseType
-            else
-               tp := UsualUnaryConversions
+      {do an indirect ++ or --}
+      LoadAddress(tree);                {get the address to save to}
+      if expressionType^.kind = arrayType then
+         expressionType := expressionType^.aType
+      else if expressionType^.kind = pointerType then
+         expressionType := expressionType^.pType;
+      if expressionType^.kind = scalarType then
+         if expressionType^.baseType in 
+            [cgByte,cgUByte,cgWord,cgUWord,cgReal,cgDouble,cgComp,cgExtended] then
+            tp := expressionType^.baseType
          else
-            tp := UsualUnaryConversions;
-         if (tp in [cgByte,cgUByte,cgWord,cgUword])
-            and (expressionType^.cType <> ctBool) then
-            Gen0t(pc_i, tp)             {do indirect inc/dec}
-         else begin
-            t1 := GetTemp(cgLongSize);
-            Gen2t(pc_str, t1, 0, cgULong);
-            Gen2t(pc_lod, t1, 0, cgULong);
-            Gen2t(pc_lod, t1, 0, cgULong);
-            FreeTemp(t1, cgLongSize);
-            Gen1t(pc_ind, 0, tp);       {load the value}
-            if pc_l in [pc_lli,pc_lld] then
-               if expressionType^.cType = ctBool then begin
-                  t1 := GetTemp(cgWordSize);
-                  Gen2t(pc_cop, t1, 0, cgWord);
-                  end; {if}
-            IncOrDec(pc_l in [pc_lli,pc_lil]); {do the ++ or --}
-            if isBitField then          {copy the value}
-               if bitDisp+bitSize > 16 then begin
-                  Gen2t(pc_cbf, bitDisp, bitSize, cgLong);
-                  Gen0t(pc_bno, cgLong);
-                  end {if}
-               else begin
-                  Gen2t(pc_cbf, bitDisp, bitSize, cgWord);
-                  Gen0t(pc_bno, cgWord);
-                  end {else}
-            else begin
-               Gen0t(pc_cpi, tp);
-               Gen0t(pc_bno, tp);
-               end; {else}
-            if pc_l in [pc_lli,pc_lld] then {correct the value for postfix ops}
-               if expressionType^.cType = ctBool then begin
-                  Gen0t(pc_pop, cgWord);
-                  Gen2t(pc_lod, t1, 0, cgWord);
-                  Gen0t(pc_bno, cgWord);
-                  FreeTemp(t1, cgWordSize);
-                  end {if}
-               else
-                  IncOrDec(pc_l = pc_lld);
-            end; {else}
+            tp := UsualUnaryConversions
+      else
+         tp := UsualUnaryConversions;
+      if (tp in [cgByte,cgUByte,cgWord,cgUword])
+         and (expressionType^.cType <> ctBool)
+         and not isBitField then
+         Gen0t(pc_i, tp)                {do indirect inc/dec}
+      else begin
+         t1 := GetTemp(cgLongSize);
+         Gen2t(pc_str, t1, 0, cgULong);
+         Gen2t(pc_lod, t1, 0, cgULong);
+         Gen2t(pc_lod, t1, 0, cgULong);
+         FreeTemp(t1, cgLongSize);
+                                        {load the value}
+         if isBitField then begin
+            if unsigned then
+               Gen2t(pc_lbu, bitDisp, bitSize, tp)
+            else
+               Gen2t(pc_lbf, bitDisp, bitSize, tp);
+            end {if}
+         else
+            Gen1t(pc_ind, 0, tp);
+         if pc_l in [pc_lli,pc_lld] then
+            if expressionType^.cType = ctBool then begin
+               t1 := GetTemp(cgWordSize);
+               Gen2t(pc_cop, t1, 0, cgWord);
+               end; {if}
+         IncOrDec(pc_l in [pc_lli,pc_lil]); {do the ++ or --}
+         if isBitField then             {copy the value}
+            Gen2t(pc_cbf, bitDisp, bitSize, tp)
+         else
+            Gen0t(pc_cpi, tp);
+         Gen0t(pc_bno, tp);
+         if pc_l in [pc_lli,pc_lld] then {correct the value for postfix ops}
+            if expressionType^.cType = ctBool then begin
+               Gen0t(pc_pop, cgWord);
+               Gen2t(pc_lod, t1, 0, cgWord);
+               Gen0t(pc_bno, cgWord);
+               FreeTemp(t1, cgWordSize);
+               end {if}
+            else
+               IncOrDec(pc_l = pc_lld);
          end; {else}
+      end; {else}
 1:
    end; {DoIncDec}
 

commit a646a03b5e379327a51144921928843d8feb41c3

Avoid possible errors when using postfix ++/-- on pointer expressions.
There was code that would attempt to use the cType field of the type record, but this is only valid for scalar types, not pointer types. In the case of a pointer type, the upper two bytes of the pointer would be interpreted as a cType value, and if they happened to have one of the values being tested for, incorrect intermediate code would be generated. The lower two bytes of the pointer would be used as a baseType value; this would most likely result in "compiler error" messages from the code generator, but might cause incorrect code generation with no errors if that value happened to correspond to a real baseType.

Code like the following might cause this error, although it only occurs if pointers have certain values and therefore depends on the memory layout at compile time:

void f(const int **p) {
    (*p)++;
}

This bug was introduced in commit f2a66a524a23d.

Expression.pas
@@ -3314,8 +3314,10 @@ var
          else
             Gen2t(pc_ind, ord(tqVolatile in expressionType^.qualifiers), 0, tp);
          if pc_l in [pc_lli,pc_lld] then
-            if expressionType^.cType in [ctBool,ctFloat,ctDouble,ctLongDouble,
-               ctComp] then begin
+            if (expressionType^.kind = scalarType) and
+               (expressionType^.cType in
+                  [ctBool,ctFloat,ctDouble,ctLongDouble,ctComp])
+               then begin
                t1 := GetTemp(ord(expressionType^.size));
                Gen2t(pc_cop, t1, 0, expressionType^.baseType);
                end; {if}
@@ -3326,8 +3328,10 @@ var
             Gen0t(pc_cpi, tp);
          Gen0t(pc_bno, tp);
          if pc_l in [pc_lli,pc_lld] then {correct the value for postfix ops}
-            if expressionType^.cType in [ctBool,ctFloat,ctDouble,ctLongDouble,
-               ctComp] then begin
+            if (expressionType^.kind = scalarType) and
+               (expressionType^.cType in
+                  [ctBool,ctFloat,ctDouble,ctLongDouble,ctComp])
+               then begin
                Gen0t(pc_pop, expressionType^.baseType);
                Gen2t(pc_lod, t1, 0, expressionType^.baseType);
                Gen0t(pc_bno, expressionType^.baseType);

cc.notes
@@ -1598,6 +1598,8 @@ If you use #pragma debug 0x0010 to enable stack check debug code, the compiler w
 
 4.  A compile error is no longer reported for code that divides an integer constant by zero (e.g. 1/0).  Such code will produce undefined behavior if it is executed, but since the compiler cannot always determine whether the code will be executed at run time, this is no longer treated as an error that prevents compilation.  If #pragma lint bit 5 is set, a lint message about the division by zero will still be produced.  An error will also still be reported for division by zero in constant expressions that need to be evaluated at compile time.  
 
+5.  In some obscure circumstances, ORCA/C might behave incorrectly when the postfix ++ or -- operators were used on an expression of pointer type.  This would typically result in "compiler error" messages, but could potentially also cause incorrect code to be generated without any errors being reported.
+
 -- Bugs from C 2.1.1 B3 that have been fixed in C 2.2.0 ---------------------
 
 1.  There were various bugs that could cause incorrect code to be generated in certain cases.  Some of these were specific to certain optimization passes, alone or in combination.