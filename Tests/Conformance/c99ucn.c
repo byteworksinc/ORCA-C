bccacd455573beb8539b561c433d56252603bac9
Stephen Heumann <stephenheumann@gmail.com>
2026-06-20 19:58:38 -0500

    Update UCN test case to follow C23 rules for UCNs in identifiers.
    Note that C99, C11/C17, and C23 each have different rules for allowable UCNs in identifiers. This test is now compatible with the C11/C17 or C23 rules, but not the C99 ones.

--- Tests/Conformance/c99ucn.c
+++ Tests/Conformance/c99ucn.c
@@ -1,26 +1,28 @@
 /*
  * Test universal character names (C99).
+ *
+ * Note: This assumes C11 or C23 rules for UCNs allowed in identifiers.
  */
 
 #include <stdio.h>
 #include <string.h>
 
-int \u00c0\u0300\U0000aBcDaaa123\U000100aB\U000afffd = 38;
+int \u00c0\u0300\U0000aBcDaaa123\U000100aB\U000323af = 38;
 
-int abc\u2465\U00021a34xyz(void) {
+int abc\u2185\U00021a34xyz(void) {
         return '\U00000060';
 }
 
 int main(void) {
         int a;
         
-        a = \u00c0\u0300\uAbCdaaa123\U000100aB\U000a\
-fffD;
+        a = \u00c0\u0300\uAbCdaaa123\U000100aB\U0003\
+23aF;
 
         if (a != 38)
                 goto Fail;
 
-        char c = abc\U00002465\U00021a34xyz();
+        char c = abc\U00002185\U00021a34xyz();
 
         if (c != '`')
                 goto Fail;
